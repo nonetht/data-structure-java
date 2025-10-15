44581728576bf21ce80f1224f67634a23420c0da
yang <94279769+nonetht@users.noreply.github.com>
2025-10-15 20:53:05 +0800

    first commit and implement BST

---
+++ src/main/java/Tree/BST.java
@@ -0,0 +1,31 @@
+package Tree;
+
+/**
+ * 今天自己写一个二叉搜索树，就当是回顾和学习LeetCode内容了。
+ */
+public class BST<T> {
+    private TreeNode root;
+    private int size;
+
+    // 二叉树应该具备添加新的节点的功能
+    // 首先，要保证插入的数值的正确性，如果为null，则失败
+    // 如果插入相同的数值，则代表插入失败；反之，则代表插入成功
+    public boolean insert(T val) {
+
+    }
+
+    // 还应该具备查看对应元素是否存在的功能
+    public boolean contains(T val) {
+        return false;
+    }
+
+    // 具备查看BST节点数量的功能
+    public int size() {
+        return 0;
+    }
+
+    public
+}
+
+
+k
---
+++ src/main/java/Tree/TreeNode.java
@@ -0,0 +1,11 @@
+package Tree;
+
+/**
+ * 定义了TreeNode，也就是树的节点的定义
+ * @param <T>
+ */
+public class TreeNode<T> {
+    private T val;
+    private TreeNode left;
+    private TreeNode right;
+}
