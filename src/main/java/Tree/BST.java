d49a166d20bc4e06e4673d8aae71f90119ba82dd
yang <94279769+nonetht@users.noreply.github.com>
2025-10-15 21:19:27 +0800

    finish add command

--- src/main/java/Tree/BST.java
+++ src/main/java/Tree/BST.java
@@ -3,15 +3,39 @@ package Tree;
 /**
  * 今天自己写一个二叉搜索树，就当是回顾和学习LeetCode内容了。
  */
-public class BST<T> {
+public class BST<T extends Comparable<T>> {
     private TreeNode root;
     private int size;
 
     // 二叉树应该具备添加新的节点的功能
-    // 首先，要保证插入的数值的正确性，如果为null，则失败
+    // 首先，要保证插入的数值的合理性，如果为null，则失败
     // 如果插入相同的数值，则代表插入失败；反之，则代表插入成功
-    public boolean insert(T val) {
+    public void insert(T val) {
+        if (val == null) {
+            // 如何弹出错误消息：invalid value
+            return;
+        }
+        add(root, val);
+    }
 
+    // insert函数的辅助函数
+    // 类似于add函数，
+    private TreeNode<T> add(TreeNode<T> node, T val) {
+        if (node == null) {
+            return new TreeNode<>(val);
+        }
+
+        int cmp = val.compareTo(node.val);
+        if (cmp > 0) {
+            // go right
+            node.right = add(node.right, val);
+        } else if (cmp < 0) {
+            // go left
+            node.left = add(node.left, val);
+        } else {
+            // do nothing
+            return null;
+        }
     }
 
     // 还应该具备查看对应元素是否存在的功能
@@ -24,8 +48,13 @@ public class BST<T> {
         return 0;
     }
 
-    public
+    /**
+     * 整个BST之中最为复杂的操作，需要分三种情况来讨论
+     * @param val
+     */
+    public void remove(T val) {
+
+    }
+
+
 }
-
-
-k
--- src/main/java/Tree/TreeNode.java
+++ src/main/java/Tree/TreeNode.java
@@ -4,8 +4,14 @@ package Tree;
  * 定义了TreeNode，也就是树的节点的定义
  * @param <T>
  */
-public class TreeNode<T> {
-    private T val;
-    private TreeNode left;
-    private TreeNode right;
+public class TreeNode<T extends Comparable<T>> {
+    T val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(T val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
